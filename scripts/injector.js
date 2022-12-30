83d0e639b2bafc89304eb4e2f5b44e3bf71c628e
Purkit8075 <purkit@outlook.com>
2022-12-30 18:08:47 +0800

    add umami

---
+++ scripts/injector.js
@@ -0,0 +1,2 @@
+// <博客项目根目录>/scripts/injector.js
+hexo.extend.injector.register('head_end', '<script async defer data-website-id="02d6c530-770d-4f3b-accd-62b55c0a5153" src="https://analyze.purkit.ml/umami.js"></script>');
