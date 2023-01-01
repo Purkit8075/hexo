5a0f7c409e6539efdcaf8bcb0878c7c1986b5194
Purkit8075 <purkit@outlook.com>
2023-01-01 14:13:05 +0800

    umami change from netlify to vercel

--- scripts/injector.js
+++ scripts/injector.js
@@ -1,2 +1,2 @@
 // <博客项目根目录>/scripts/injector.js
-hexo.extend.injector.register('head_end', '<script async defer data-website-id="02d6c530-770d-4f3b-accd-62b55c0a5153" src="https://analyze.purkit.ml/umami.js"></script>');
+hexo.extend.injector.register('head_end', '<script async defer data-website-id="83e6aefa-a710-46c1-85f1-26d421ae81ab" src="https://umami.purkit.ml/umami.js"></script>');
