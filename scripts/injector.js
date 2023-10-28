b356f6b21c93019bc2068f1b5c015b55c86d3a1f
Purkit <purkit@outlook.com>
2023-10-28 21:11:03 +0800

    remove analyse about comment from menu

--- scripts/injector.js
+++
@@ -1,2 +0,0 @@
-// <博客项目根目录>/scripts/injector.js
-hexo.extend.injector.register('head_end', '<script async defer data-website-id="83e6aefa-a710-46c1-85f1-26d421ae81ab" src="https://umami.purkit.ml/umami.js"></script>');
